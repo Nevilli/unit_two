0e5af293d06728ce5d9f6b889caf0e66fe781b8d
Lliam <lliam@iMac8.local>
2018-09-19 08:56:58 -0400

    Completed first few steps of code

---
+++ assignment_two.py..py
@@ -0,0 +1,8 @@
+user = input("What is your name?")
+print("hello", user)
+print("It's so nice to meet you. My name is Chad")
+location = input("Where are you from?")
+print("Sounds like a god awful to live")
+negative_question = input("How can you live in such a dump?")
+negative_response = input("Whatever. Do you have any questions for me?")
+print()
