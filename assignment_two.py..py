aa0d9b9c7100b9f683ff9d1aa7283d7545fb6bd4
Lliam <lliam@iMac8.local>
2018-09-20 10:17:22 -0400

    almost finished with monthly payment

--- assignment_two.py..py
+++ assignment_two.py..py
@@ -4,5 +4,18 @@ print("It's so nice to meet you. My name is Chad")
 location = input("Where are you from?")
 print("Sounds like a god awful to live")
 negative_question = input("How can you live in such a dump?")
-negative_response = input("Whatever. Do you have any questions for me?")
-print()
+number = float(input("Whatever. What is your favorite number?"))
+print("My favorite number is 8")
+print("Your number minus my number is", number - 8)
+car = input("I have another question. What is your dream car?")
+print("Oh, that's interesting.")
+cost = float(input("What is the price of that car?"))
+print("That is a reasonably priced car")
+car_loan = float(input("How many years would it take to pay off a loan for that car?"))
+rate = float(input("What would be the annual interest rate for the car?"))
+rate = rate / 100 / 12
+car_loan = car_loan * 12
+monthly_payment = (rate * car_loan) / (1 - (1 + rate) ** -car_loan)
+
+
+
