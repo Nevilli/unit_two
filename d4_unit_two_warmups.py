1193496469d3cdf24efe34a4514e7fe75c656a5e
Lliam <lliam@iMac8.local>
2018-09-20 09:15:23 -0400

    Mathematical operators

---
+++ d4_unit_two_warmups.py
@@ -0,0 +1,12 @@
+print(9 * 5)
+print(2 / 5)
+print(12 // 5)
+print(27 // 4)
+print(2 // 5)
+print(5 % 2)
+print(9 % 5)
+print(6 % 6)
+print(2 % 7)
+
+# The // is how many times the second number fits into the first or integer division
+# The % is the remainder after dividing the two numbers 
